f1487355fe81955701bbf0f1a02ce259cafca7bd
Alisson Deivison <uepbalisson@gmail.com>
2023-08-24 07:15:36 -0300

    Terceira semana, primeiro dia

---
+++ 08-23-2023/main.py
@@ -0,0 +1,6 @@
+import requests
+requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+print(requisicao)
+print(requisicao.json())
+dir_requisicao = requisicao.json()
+print(dir_requisicao['USDBRL']['bid'])
