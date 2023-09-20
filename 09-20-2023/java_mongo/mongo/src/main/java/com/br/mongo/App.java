5929f5e8a5d69e4cfb8234feda7e7de43653728f
Alisson Deivison <uepbalisson@gmail.com>
2023-09-20 20:50:04 -0300

    MongoDB - Parte 1

---
+++ 09-20-2023/java_mongo/mongo/src/main/java/com/br/mongo/App.java
@@ -0,0 +1,13 @@
+package com.br.mongo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
